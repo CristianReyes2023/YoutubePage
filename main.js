706dd67c0443159ff20562cd364d05f0ed9e5726
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-08-26 22:11:02 -0500

    feat: :sparkles: Added the main container videos
    Added the main container videos in the index file

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let menuIcon = document.querySelector(".menu-icon");
 let sidebar = document.querySelector(".sidebar");
 
-menuIcon.onclick = function(){
-    sidebar.classList.toggle("small-sidebar");
-}
+menuIcon.addEventListener('click',()=>{
+    sidebar.classList.toggle('small-sidebar')
+})
+// menuIcon.onclick = function(){
+//     sidebar.classList.toggle("small-sidebar");
+// }
