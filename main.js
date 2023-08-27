7dd7cb800b50b505fd943477e9f2d7a3ab06379e
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-08-27 16:04:43 -0500

    feat: :sparkles: Added api.js file
    Added api.js file and created the build videos function

--- main.js
+++ main.js
@@ -9,44 +9,62 @@ menuIcon.addEventListener('click', () => {
 
 
 
+
 const path = "jsonVideos";
 
 let construirVideos = async () => {
     let peticion = await fetch(`${path}.json`);
     let res = await peticion.json();
     let selecion = document.querySelector("#myVideos");
-    selecion.insertAdjacentHTML("beforeend",/*html*/ `
+    selecion.insertAdjacentHTML("beforeend", /*html*/`
     <div class="list-video">
-    ${res.contents.videos.thumbnails.map((value) => /*html*/ `
-        <a href="index-playvideo.html"><img src="${value.url}" alt="videos" class="img-miniatura"/></a>
-        `).join(" ")}
-        <div class="video-browser ">
-            <img src="images/CreativeCode.jpg" alt="usuario"/>
-            ${res.contents.videos.map((value) => /*html*/ `
-                <div class="video-info">
-                    <a href="index-playvideo.html">${videos.title}</a>
-                    <p></p>
-                    <p>${value.stats.views} Views &bull; ${value.publishedTimeText}</p>
-                </div>`).join(" ")}
+    ${res.contents.map((video) => /*html*/ `
+        <div class="thumbnails">
+            ${video.video.thumbnails.map((thumbnail) => /*html*/ `
+                <img src="${thumbnail.url}" alt="videos" class="img-miniatura"/>
+            `).join(" ")}
         </div>
-    </div>`)
+        <div class="video-info">
+            <a href="index-playvideo.html">${video.video.title}</a>
+            <p>${video.video.stats.views} Views &bull; ${video.video.publishedTimeText}</p>
+        </div>
+    `).join(" ")}
+    </div>
+    `);
 }
+
 construirVideos();
 
+// const path = "jsonVideos";
 
-// const url = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UC8fkwsjcI_MhralEX1g4OBw&hl=en&gl=US';
-// const options = {
-// 	method: 'GET',
-// 	headers: {
-// 		'X-RapidAPI-Key': '67bc891487msh83d7f6087e5baa4p137206jsn95c6efd2fc62',
-// 		'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
-// 	}
-// };
-
-// try {
-// 	const response = await fetch(url, options);
-// 	const result = await response.text();
-// 	console.log(result);
-// } catch (error) {
-// 	console.error(error);
+// let construirVideos = async () => {
+//     let peticion = await fetch(`${path}.json`);
+//     let res = await peticion.json();
+//     let selecion = document.querySelector("#myVideos");
+//     selecion.insertAdjacentHTML("beforeend", `
+//     <div class="list-video">
+//     ${res.contents.map((video) => /*html*/ `
+//         <div class="thumbnails">
+//             ${video.video.thumbnails.map((thumbnail) => /*html*/ `
+//                 <img src="${thumbnail.url}" alt="videos" class="img-miniatura"/>
+//             `).join(" ")}
+//         </div>
+//         <div class="video-info">
+//             <a href="index-playvideo.html">${video.video.title}</a>
+//             <p>${video.video.stats.views} Views &bull; ${video.video.publishedTimeText}</p>
+//         </div>
+//     `).join(" ")}
+//     </div>
+//     `);
 // }
+
+// construirVideos();
+
+
+
+
+
+
+
+
+
