e13966761936a6a09ae5f49f520bac0244a9b1e0
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-08-27 11:24:00 -0500

    feat: :sparkles: Added main.js file
    Added main.js file, video creation using youtube API.

--- main.js
+++ main.js
@@ -2,13 +2,36 @@ let menuIcon = document.querySelector(".menu-icon");
 let sidebar = document.querySelector(".sidebar");
 let container = document.querySelector(".main-container");
 
-menuIcon.addEventListener('click',()=>{
+menuIcon.addEventListener('click', () => {
     sidebar.classList.toggle('small-sidebar')
     container.classList.toggle('large-container')
 })
 
 
 
+const path = "jsonVideos";
+
+let construirVideos = async () => {
+    let peticion = await fetch(`${path}.json`);
+    let res = await peticion.json();
+    let selecion = document.querySelector("#myVideos");
+    selecion.insertAdjacentHTML("beforeend",/*html*/ `
+    <div class="list-video">
+    ${res.contents.videos.thumbnails.map((value) => /*html*/ `
+        <a href="index-playvideo.html"><img src="${value.url}" alt="videos" class="img-miniatura"/></a>
+        `).join(" ")}
+        <div class="video-browser ">
+            <img src="images/CreativeCode.jpg" alt="usuario"/>
+            ${res.contents.videos.map((value) => /*html*/ `
+                <div class="video-info">
+                    <a href="index-playvideo.html">${videos.title}</a>
+                    <p></p>
+                    <p>${value.stats.views} Views &bull; ${value.publishedTimeText}</p>
+                </div>`).join(" ")}
+        </div>
+    </div>`)
+}
+construirVideos();
 
 
 // const url = 'https://youtube138.p.rapidapi.com/channel/videos/?id=UC8fkwsjcI_MhralEX1g4OBw&hl=en&gl=US';
